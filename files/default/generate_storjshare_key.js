9a4a3190aa96649753f30eeeaf0a1c86be4d5e4e
Philip Hutchins <flipture@gmail.com>
2016-06-22 10:07:53 -0400

    Adding newline

--- files/default/generate_storjshare_key.js
+++ files/default/generate_storjshare_key.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 var storj = require('storj');
 var fs = require('fs');
+
 var key = storj.KeyPair().getPrivateKey();
 var password = process.env.PASSWORD;
 var encryptedKey = storj.utils.simpleEncrypt(password, key);
